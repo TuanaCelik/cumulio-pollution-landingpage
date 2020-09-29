e58d4d1d05ecd6585caf938e9b77669c186b2cd6
Tuana Celik <tuanacelik95@gmail.com>
2020-09-29 17:49:59 +0100

    initial integration

---
+++ public/js/app.js
@@ -0,0 +1,51 @@
+// dashboard configuration for integration
+const dashboardId = 'e830af8d-bf1d-4918-963e-884b6c0fc569';
+
+const dashboardOptions = {
+  dashboardId: dashboardId,
+  container: '#dashboard-container',
+  loader: {
+    background: '#EEF3F6',
+    spinnerColor: '#004CB7',
+    spinnerBackground: '#DCDCDC',
+    fontColor: '#000000'
+  }
+}
+
+// Function to add the dashboard to the page using Cumul.io embed
+const loadDashboard = (key, token) => {
+  // use tokens if available
+  if (key && token) {
+    dashboardOptions.key = key;
+    dashboardOptions.token = token;
+  }
+  // add the dashboard to the #dashboard-container element
+  Cumulio.addDashboard(dashboardOptions);
+}
+
+// function to load the insight page
+const loadInsightsPage = async () => {
+  const response = await fetch('/authorization', {});
+  const responseData = await response.json();
+  console.log(responseData.CUMULIO_API_KEY);
+  if (responseData.CUMULIO_API_KEY && responseData.CUMULIO_API_TOKEN) {
+    loadDashboard(responseData.CUMULIO_API_KEY, responseData.CUMULIO_API_TOKEN);
+  }
+}
+
+const toggleMenu = (boolean) => {
+  if (boolean) {
+    document.getElementById('sidebar').classList.add('open');
+    document.getElementById('overlay').classList.add('open');
+  }
+  else {
+    document.getElementById('sidebar').classList.remove('open');
+    document.getElementById('overlay').classList.remove('open');
+  }
+}
+
+// on page load
+window.onload = async () => {
+  console.log("Got to window load");
+  loadInsightsPage();
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const { join } = require('path');
+const app = express();
+const Cumulio = require('cumulio');
+// Listen on port 3000
+const dashboardId = 'e830af8d-bf1d-4918-963e-884b6c0fc569';
+
+// const client = new Cumulio({
+//     api_key: process.env.CUMULIO_API_KEY,
+//     api_token: process.env.CUMULIO_API_TOKEN
+// });
+
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/authorization', (req, res) => {
+    // client.create('authorization', {
+    //     type: 'temporary',
+    //     expiry: '1 day',
+    //     inactivity_interval: '30 minutes',
+    //     securables: [dashboardId]
+    //   }).then((result) => {
+    //     return res.status(200).json(result);
+    // });
+    return res.status(200).json(process.env);
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => console.log('Application running on port 3000'));
